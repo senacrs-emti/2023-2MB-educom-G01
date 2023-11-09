0347c5fdd809d24d84c7139c0379abf4426ede67
adrielyrossetti <adrielyfragarossetti@gmail.com>
2023-11-09 10:06:53 -0300

    banco

--- quimico.sql
+++ quimico.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 19-Out-2023 às 14:50
+-- Tempo de geração: 07-Nov-2023 às 16:22
 -- Versão do servidor: 10.4.17-MariaDB
 -- versão do PHP: 7.3.27
 
@@ -29,9 +29,18 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `questoes` (
   `id` int(11) NOT NULL,
-  `Pontos` varchar(45) NOT NULL,
-  `Respotas_id` int(11) NOT NULL
+  `Pontos` int(11) NOT NULL,
+  `equacao` varchar(45) NOT NULL,
+  `Respostas_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Extraindo dados da tabela `questoes`
+--
+
+INSERT INTO `questoes` (`id`, `Pontos`, `equacao`, `Respostas_id`) VALUES
+(101, 100, 'K2Cr2O7(aq) + H2O(l) + S(g) → KOH(aq) + Cr2O3', 201),
+(103, 100, ' KMnO4(aq) + FeCl2(aq) + HCl(aq) →  MnCl2(aq)', 203);
 
 -- --------------------------------------------------------
 
@@ -42,7 +51,7 @@ CREATE TABLE `questoes` (
 CREATE TABLE `responde` (
   `id` int(11) NOT NULL,
   `Questoes_id` int(11) NOT NULL,
-  `Respotas_id` int(11) NOT NULL
+  `Respostas_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -52,10 +61,19 @@ CREATE TABLE `responde` (
 --
 
 CREATE TABLE `respostas` (
-  `id` int(11) NOT NULL,
-  `Resposta certa` varchar(45) DEFAULT NULL,
-  `Resposta errada` varchar(45) DEFAULT NULL
+  `id` int(11) NOT NULL DEFAULT 0,
+  `RespostaCerta` varchar(45) DEFAULT NULL,
+  `RespostaErrada` varchar(45) DEFAULT NULL,
+  `RespostaErrada2` varchar(45) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Extraindo dados da tabela `respostas`
+--
+
+INSERT INTO `respostas` (`id`, `RespostaCerta`, `RespostaErrada`, `RespostaErrada2`) VALUES
+(201, '2 K2Cr2O7(aq) + 2 H2O(l) + 3 S(g) → 4 KOH(aq)', '2 K2Cr2O7(aq) + 2 H2O(l) + 2 S(g) → 2 KOH(aq)', ' K2Cr2O7(aq) + 1 H2O(l) + S(g) → 2 KOH(aq)'),
+(203, '1 KMnO4(aq) + 5 FeCl2(aq) + 8 HCl(aq) → 1 MnC', '2 KMnO4(aq) + 3 FeCl2(aq) + 2 HCl(aq) → 4 MnC', '1 KMnO4(aq) + 3 FeCl2(aq) + 3 HCl(aq) → 2 MnC');
 
 -- --------------------------------------------------------
 
@@ -68,7 +86,8 @@ CREATE TABLE `usuario` (
   `Nickname` varchar(45) NOT NULL,
   `Pontos` int(11) NOT NULL,
   `Tempo` time NOT NULL,
-  `Responde_id` int(11) NOT NULL
+  `Responde_id` int(11) NOT NULL,
+  `Email` varchar(45) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
@@ -80,15 +99,15 @@ CREATE TABLE `usuario` (
 --
 ALTER TABLE `questoes`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `fk_Questões_Respotas1` (`Respotas_id`);
+  ADD KEY `fk_Questoes_Respostas1` (`Respostas_id`);
 
 --
 -- Índices para tabela `responde`
 --
 ALTER TABLE `responde`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `fk_Responde_Questões1` (`Questoes_id`),
-  ADD KEY `fk_Responde_Respotas1` (`Respotas_id`);
+  ADD KEY `fk_Responde_Questoes1` (`Questoes_id`),
+  ADD KEY `fk_Responde_Respostas1` (`Respostas_id`);
 
 --
 -- Índices para tabela `respostas`
@@ -101,30 +120,18 @@ ALTER TABLE `respostas`
 --
 ALTER TABLE `usuario`
   ADD PRIMARY KEY (`idUsuario`),
-  ADD KEY `fk_Usuário_Responde1` (`Responde_id`);
+  ADD KEY `fk_Usuario_Responde1` (`Responde_id`);
 
 --
 -- Restrições para despejos de tabelas
 --
 
 --
--- Limitadores para a tabela `questoes`
---
-ALTER TABLE `questoes`
-  ADD CONSTRAINT `fk_Questões_Respotas1` FOREIGN KEY (`Respotas_id`) REFERENCES `respostas` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
-
---
 -- Limitadores para a tabela `responde`
 --
 ALTER TABLE `responde`
-  ADD CONSTRAINT `fk_Responde_Questões1` FOREIGN KEY (`Questoes_id`) REFERENCES `questoes` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  ADD CONSTRAINT `fk_Responde_Respotas1` FOREIGN KEY (`Respotas_id`) REFERENCES `respostas` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
-
---
--- Limitadores para a tabela `usuario`
---
-ALTER TABLE `usuario`
-  ADD CONSTRAINT `fk_Usuário_Responde1` FOREIGN KEY (`Responde_id`) REFERENCES `responde` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+  ADD CONSTRAINT `fk_Responde_Questoes1` FOREIGN KEY (`Questoes_id`) REFERENCES `questoes` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_Responde_Respostas1` FOREIGN KEY (`Respostas_id`) REFERENCES `respostas` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
