d71a4bb017997d2b2b4c2440bec2891eb1d66e60
adrielyrossetti <adrielyfragarossetti@gmail.com>
2023-11-17 09:17:41 -0300

    inserção de todas as questoes e respostas de equacoes no banco

--- quimico.sql
+++ quimico.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 14-Nov-2023 às 16:16
+-- Tempo de geração: 17-Nov-2023 às 13:14
 -- Versão do servidor: 10.4.17-MariaDB
 -- versão do PHP: 7.3.27
 
@@ -49,7 +49,15 @@ INSERT INTO `questoes` (`id`, `Pontos`, `equacao`, `Respostas_id`) VALUES
 (109, 100, 'C3H8 + O2 → CO2 + H2O', 209),
 (110, 100, 'Na2SO4 + BaCl2 → BaSO4 + NaCl', 210),
 (111, 100, 'Fe2O3 + H2 → Fe + H2O', 211),
-(112, 100, 'C2H5OH + O2 → CO2 + H2O', 212);
+(112, 100, 'C2H5OH + O2 → CO2 + H2O', 212),
+(113, 100, 'Mg + HCl → MgCl2 + H2', 213),
+(114, 100, 'Al + H2SO4 → Al2(SO4)3 + H2', 214),
+(115, 100, 'BaO4 + HNO3 → Ba(NO3)2 + H2O2 + O2', 215),
+(116, 100, 'SO2 + NaOH → Na2SO3 + H2O', 216),
+(117, 100, 'KClO4 → KCl+ O2', 217),
+(118, 100, 'CS2 + H2S + Cu → Cu2S + CH4', 218),
+(119, 100, 'Br2 + NaOH → NaBr + NaBrO3 + H2O', 219),
+(120, 100, 'Co2 + BrO + H → Co3 + Br2 + H2O', 220);
 
 -- --------------------------------------------------------
 
@@ -72,7 +80,21 @@ INSERT INTO `responde` (`id`, `Questoes_id`, `Respostas_id`) VALUES
 (3003, 103, 203),
 (3004, 104, 204),
 (3005, 105, 205),
-(3006, 106, 206);
+(3006, 106, 206),
+(3007, 107, 207),
+(3008, 108, 208),
+(3009, 109, 209),
+(3010, 110, 210),
+(3011, 111, 211),
+(3012, 112, 212),
+(3013, 113, 213),
+(3014, 114, 214),
+(3015, 115, 215),
+(3016, 116, 216),
+(3017, 117, 217),
+(3018, 118, 218),
+(3019, 119, 219),
+(3020, 120, 220);
 
 -- --------------------------------------------------------
 
@@ -103,7 +125,14 @@ INSERT INTO `respostas` (`id`, `RespostaCerta`, `RespostaErrada`, `RespostaErrad
 (210, '1 Na2SO4 + 1 BaCl2 → 1 BaSO4 + 2 NaCl', '2 Na2SO4 + 4 BaCl2 → 2 BaSO4 + 3 NaCl', '2 Na2SO4 + 4 BaCl2 → 3 BaSO4 + 2 NaCl'),
 (211, '1 Fe2O3 + 3 H2 → 2 Fe + 3 H2O', '2 Fe2O3 + 1 H2 → 2 Fe + 2 H2O', '2 Fe2O3 + 2 H2 → 2 Fe + 4 H2O'),
 (212, '1 C2H5OH + 3 O2 → 2 CO2 + 3 H2O', '3 C2H5OH + 3 O2 → 3 CO2 + 2 H2O', '2 C2H5OH + 3 O2 → 4 CO2 + 2 H2O'),
-(213, '1 C3H8 + 5 O2 → 3 CO2 + 4 H2O', '1 C3H8 + 4 O2 → 2 CO2 + 3 H2O', '1 C3H8 + 3 O2 → 2 CO2 + 3 H2O');
+(213, '1 Mg + 2 HCl → 1 MgCl2 + 1 H2', '2 Mg + 2 HCl → 1 MgCl2 + 1 H2', '2 Mg + 1 HCl → 1 MgCl2 + 2 H2'),
+(214, '2 Al + 3 H2SO4 → 1 Al2(SO4)3 + 3 H2', '3 Al + 2 H2SO4 → 2 Al2(SO4)3 + 3 H2', '3 Al + 2 H2SO4 → 1 Al2(SO4)3 + 4 H2'),
+(215, '1 BaO4 + 2 HNO3 → 1 Ba(NO3)2 + 1 H2O2 + 1 O2', '1 BaO4 + 2 HNO3 → 2 Ba(NO3)2 + 2 H2O2 + 1 O2', '1 BaO4 + 2 HNO3 → 2 Ba(NO3)2 + 3 H2O2 + 1 O2'),
+(216, '1 SO2 + 2 NaOH → 1 Na2SO3 + 1 H2O', '2 SO2 + 2 NaOH → 1 Na2SO3 + 2 H2O', '2 SO2 + 1 NaOH → 1 Na2SO3 + 1 H2O'),
+(217, '1 KClO4 → 1 KCl+ 2 O2', '1 KClO4 → 4 KCl+ 2 O2', '2 KClO4 → 4 KCl+ 2 O2'),
+(218, '1 CS2 + 2 H2S + 8 Cu → 4 Cu2S + 1 CH4', '1 CS2 + 2 H2S + 3 Cu → 3 Cu2S + 2 CH4', '3 CS2 + 2 H2S + 8 Cu → 1 Cu2S + 2 CH4'),
+(219, '3 Br2 + 6 NaOH → 5 NaBr + 1 NaBrO3 + 3 H2O', '3 Br2 + 4 NaOH → 5 NaBr + 2 NaBrO3 + 1 H2O', '3 Br2 + 2 NaOH → 1 NaBr + 2 NaBrO3 + 2 H2O'),
+(220, '6 Co2+ + 2 BrO- + 4 H → 4 Co3+ + 1 Br2 + 2 H2', '1 Co2+ + 2 BrO- + 2 H → 1 Co3+ + 1 Br2 + 2 H2', '6 Co2+ + 2 BrO- + 4 H → 4 Co3+ + 2 Br2 + 1 H2');
 
 -- --------------------------------------------------------
 
