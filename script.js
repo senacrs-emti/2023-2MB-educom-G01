15cc462e62a466d819b378b3e32413d0dd289a2d
FelipeMYung <102309048+FelipeMYung@users.noreply.github.com>
2023-11-23 11:03:20 -0300

    aa

--- script.js
+++ script.js
@@ -3,14 +3,14 @@ const context1 = canvas_esquerda.getContext("2d");
 
 const canvas_direita = document.getElementById("canvas_direita");
 const context2 = canvas_direita.getContext("2d");
-
+let atomos = document.getElementsByClassName('esquerda').value;
 function somarEsquerda() {
     let soma = 0;
-    let input = document.getElementsByClassName('esquerda').value;
+
     // Percorre todos os inputs
-    for (let valores of input) {
+    for (let valores of atomos) {
       // Obtém o valor do input
-      const valor = Number(input.value);
+      const valor = Number(atomos.value);
   
       // Adiciona o valor à soma
       soma += valor;
